2f6acb74d428879b7a6469aaa8e4ba37fb4820c9
hudaokeji <github@hudaokeji.com>
2019-03-06 14:04:49 +0900

    update original dokuwiki to the latest master version

--- README.md
+++
@@ -1,2 +0,0 @@
-# skyphp-dokuwiki
-A version of dokuwiki that ported to running on skylarkjs and skylarkphp.
---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$WY13JM1a$YtfWzBYct/iNAk16ohgTG/:admin:admin@bbb.com:admin,user
